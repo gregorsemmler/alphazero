698ae7a25e8a4fd432e08d3be4824857eca878f0
Gregor Semmler <gregor.semmler@protonmail.com>
2021-07-18 17:24:39 +0200

    Begin adding training code

--- train.py
+++ train.py
@@ -1,6 +1,25 @@
+import logging
+
+import torch
+
+from game import ConnectNGame
+from model import CNNModel
 
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+
+    device_token = "cuda" if torch.cuda.is_available() else "cpu"
+    device = torch.device(device_token)
+
+    game = ConnectNGame()
+
+    input_shape = (2, game.n_rows, game.n_cols)
+    num_filters = 64
+    num_residual_blocks = 5
+    val_hidden_size = 20
+    model = CNNModel(input_shape, num_filters, num_residual_blocks, val_hidden_size, game.n_cols)
+
     pass
 
 
